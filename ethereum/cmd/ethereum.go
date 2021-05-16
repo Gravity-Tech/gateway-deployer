849647cf32c22ff4cbf1d98acabb5ccb9e8b6671
Shamil Khalilov <shamfama@gmail.com>
2021-05-16 20:00:43 +0300

    Update dependencies

--- ethereum/cmd/ethereum.go
+++ ethereum/cmd/ethereum.go
@@ -2,13 +2,11 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 	"github.com/Gravity-Tech/gateway-deployer/ethereum/config"
 	"github.com/Gravity-Tech/gateway-deployer/ethereum/deployer"
-	"github.com/ethereum/go-ethereum/common"
-	"strconv"
-
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"os"
 
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
@@ -121,45 +119,6 @@ func deploy(ctx *cli.Context) error {
 	fmt.Printf("Port address: %s\n", port.PortAddress)
 	fmt.Printf("Nebula address: %s\n", port.NebulaAddress)
 	fmt.Printf("Token address: %s\n", port.ERC20Address)
-
-	return nil
-}
-func faucet(ctx *cli.Context) error {
-	args := ctx.Args()
-
-	cfgPath := ctx.String(ConfigFlag)
-	cfg := new(config.EthereumConfig)
-	config.ParseConfig(cfgPath, cfg)
-
-	erc20Address := args.Get(0)
-	receiver := args.Get(1)
-	amount, err := strconv.ParseInt(args.Get(2), 10, 64)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("ERC20 Address: %s\n", erc20Address)
-	fmt.Printf("Receiver: %s\n", receiver)
-	fmt.Printf("Amount: %d\n", amount)
-
-	ethClient, err := ethclient.DialContext(ctx.Context, cfg.NodeUrl)
-	if err != nil {
-		return err
-	}
-
-	privateKey, err := crypto.HexToECDSA(cfg.PrivKey)
-	if err != nil {
-		return err
-	}
-
-	ethDeployer := deployer.NewEthDeployer(ethClient, bind.NewKeyedTransactor(privateKey))
-
-	hash, err := ethDeployer.Faucet(erc20Address, receiver, amount, ctx.Context)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("Tx id: %s\n", hash)
 
 	return nil
 }
